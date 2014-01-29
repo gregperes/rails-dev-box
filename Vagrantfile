4013ab778d5de350be040134eb5abe941429210b
gregperes <gregperes84@gmail.com>
2014-01-29 14:51:08 -0200

    now with PHP5, MySQL and custom bash scripts

--- Vagrantfile
+++ Vagrantfile
@@ -1,9 +1,9 @@
 VAGRANTFILE_API_VERSION = "2"
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
-  config.vm.box = "rails-dev"
+  config.vm.box = "precise64"
   config.vm.box_url = "http://files.vagrantup.com/precise64.box"
-  config.vm.hostname = 'rails-dev'
+  config.vm.hostname = 'gregperes-dev'
   config.vm.provision :shell, :path => "bootstrap.sh"
 
   config.vm.provider :virtualbox do |vb|
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,59 +1,116 @@
 #!/usr/bin/env bash
 
-sudo su
+function printMessage() {
+	echo -e "\n=> $1 \n"
+}
 
+function printDone() {
+	echo "==> done..."
+}
+
+# apt-get updating
+printMessage "apt-get updating..."
+
+sudo apt-get update
+sudo apt-get upgrade
+
+printDone
+
+# Var declarations
 pm="apt-get install -y"
 
-echo -e "\nUsing $pm for package installation\n"
+# Installing essential tools
+printMessage "Installing essential tools"
 
-# Install build tools
-echo -e "\n=> Installing build tools..."
 sudo $pm wget curl build-essential clang
 sudo $pm bison openssl zlib1g
 sudo $pm libxslt1.1 libssl-dev libxslt1-dev
 sudo $pm libxml2 libffi-dev libyaml-dev
 sudo $pm libxslt-dev autoconf libc6-dev
 sudo $pm libreadline6-dev zlib1g-dev libcurl4-openssl-dev libtool
-echo "==> done..."
-
-echo -e "\n=> Installing libs needed for sqlite and mysql..."
 sudo $pm libsqlite3-0 sqlite3 libsqlite3-dev libmysqlclient-dev
-echo "==> done..."
+sudo $pm git-core nodejs python-software-properties
 
-echo -e "\n=> Installing nodejs..."
-sudo $pm nodejs
-echo "==> done..."
+sudo apt-get update
 
-# Install git-core
-echo -e "\n=> Installing git..."
-sudo $pm git-core
-echo "==> done..."
+printDone
 
-echo -e "\n=> Downloading Ruby $ruby_version_string \n"
+# MySQL install
+printMessage "Installing MySQL"
+
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
+
+printDone
+
+# Ruby on Rails install
+printMessage "Installing Ruby"
+
 wget http://cache.ruby-lang.org/pub/ruby/2.1/ruby-2.1.0.tar.gz
 echo -e "\n==> done..."
 echo -e "\n=> Extracting Ruby $ruby_version_string"
 tar -xzvf ruby-2.1.0.tar.gz
-cd ruby-2.1.0/
-echo "==> done..."
+cd ruby-2.1.0
 
-echo -e "\n=> Building Ruby $ruby_version_string (this will take a while)..."
 ./configure --prefix=/usr/local
 make
 make install
-echo "==> done..."
+cd ..
+rm -R ruby-2.1.0
+rm -R ruby-2.1.0.tar.gz
 
-echo -e "\n=> Updating Rubygems..."
 gem update --system --no-ri --no-rdoc
-echo "==> done..."
 
-echo -e "\n=> Installing bundler and rails..."
+printMessage "Installing Rails"
+
 gem install bundler rails -f 
-echo "==> done..."
-
-echo -e "\n=> Fix nokogiri warning with libxml2..."
 gem install nokogiri
 gem pristine nokogiri
-echo "==> done..."
 
-echo    "### Installation is complete! Enjoy! ###"
+printDone
+
+# PHP install
+printMessage "Installing PHP"
+
+sudo add-apt-repository -y ppa:ondrej/php5
+sudo apt-get update
+
+sudo $pm php5 apache2 libapache2-mod-php5 php5-curl php5-gd php5-mcrypt mysql-server-5.5 php5-mysql
+sudo $pm php5-xdebug
+
+cat << EOF | sudo tee -a /etc/php5/mods-available/xdebug.ini
+xdebug.scream=1
+xdebug.cli_color=1
+xdebug.show_local_vars=1
+EOF
+
+sudo a2enmod rewrite
+
+sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/apache2/php.ini
+sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/apache2/php.ini
+sed -i 's/AllowOverride None/AllowOverride All/' /etc/apache2/apache2.conf
+
+sudo service apache2 restart
+
+curl -sS https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
+
+printDone
+
+# Bash scripts install
+dos2unix(){
+  tr -d '\r' < "$1" > t
+  mv -f t "$1"
+}
+
+unix2dos(){
+  sed -i 's/$/\r/' "$1"
+}
+
+printMessage "Installing bash"
+
+git clone https://github.com/gregperes/bash.git /home/vagrant/.bash
+dos2unix /home/vagrant/.bash/**/*.sh
+echo 'source /home/vagrant/.bash/init.sh' >> /home/vagrant/.bash_profile
+
+printMessage "All done. Enjoy! ;)"
